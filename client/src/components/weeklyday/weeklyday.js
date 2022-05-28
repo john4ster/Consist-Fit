64101fbbc72fa014b6ed5b3902bc125e6f38d78a
john4ster <johnforsterbusiness@gmail.com>
2022-05-28 17:13:28 -0400

    styling change on weeklyview

--- client/src/components/weeklyday/weeklyday.js
+++ client/src/components/weeklyday/weeklyday.js
@@ -8,7 +8,17 @@ import axios from 'axios';
 function WeeklyDay({ day, date, workoutCompleted }) {
 
   const { userID } = useContext(AuthContext);
-  const dateString = date.toDateString().slice(3, -4)
+  const dateString = date.toDateString().slice(3, -4);
+  const today = new Date();
+
+  //See if this date is today
+  const dateIsToday = (date) => {
+    if (today.setHours(0, 0, 0, 0) === date.setHours(0, 0, 0, 0)) {
+      return true;
+    }
+    return false;
+  }
+  const isToday = dateIsToday(date);
 
   //Check or uncheck the day
   const handleCheck = () => {
@@ -27,7 +37,7 @@ function WeeklyDay({ day, date, workoutCompleted }) {
   }
 
   return (
-    <div className="WeeklyDay">
+    <div className="WeeklyDay" style={isToday ? {background: "#b8b6b2" } : {background: "white" }}>
       <div className="dayDate">
         <h2 className="day">{day}</h2>
         <p className="date">{dateString}</p>
